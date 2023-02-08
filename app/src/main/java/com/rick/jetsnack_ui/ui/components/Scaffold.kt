f71407c9d440d310b0f6fab5765373986e07872d
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-08 12:33:18 +0300

    Components: scaffold

---
+++ app/src/main/java/com/rick/jetsnack_ui/ui/components/Scaffold.kt
@@ -0,0 +1,52 @@
+package com.rick.jetsnack_ui.ui.components
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import com.rick.jetsnack_ui.ui.theme.JetsnackTheme
+
+@Composable
+fun JetsnackScaffold(
+    modifier: Modifier = Modifier,
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    topBar: @Composable (() -> Unit) = {},
+    bottomBar: @Composable (() -> Unit) = {},
+    snackbarHost: @Composable ((SnackbarHostState) -> Unit) = { SnackbarHost(it)},
+    floatingActionButton: @Composable (() -> Unit) = {},
+    floatingActionButtonPosition: FabPosition = FabPosition.End,
+    isFloatingActionButtonDocked: Boolean = false,
+    drawerContent: @Composable (ColumnScope.() -> Unit)? = null,
+    drawerShape: Shape = MaterialTheme.shapes.large,
+    drawerElevation: Dp = DrawerDefaults.Elevation,
+    drawerBackgroundColor: Color = JetsnackTheme.colors.uiBackground,
+    drawerContentColor: Color = JetsnackTheme.colors.textSecondary,
+    drawerScrimColor: Color = JetsnackTheme.colors.uiBorder,
+    backgroundColor: Color = JetsnackTheme.colors.uiBackground,
+    contentColor: Color = JetsnackTheme.colors.textSecondary,
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    Scaffold(
+        modifier = modifier,
+        scaffoldState = scaffoldState,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        snackbarHost = snackbarHost,
+        floatingActionButton = floatingActionButton,
+        floatingActionButtonPosition = floatingActionButtonPosition,
+        isFloatingActionButtonDocked = isFloatingActionButtonDocked,
+        drawerContent = drawerContent,
+        drawerShape = drawerShape,
+        drawerElevation = drawerElevation,
+        drawerBackgroundColor = drawerBackgroundColor,
+        drawerContentColor = drawerContentColor,
+        drawerScrimColor = drawerScrimColor,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        content = content
+    )
+}
