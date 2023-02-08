92fbb5bc9da8ff7b29bfe4bf7c7517882dc90c1f
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-08 12:55:10 +0300

    Components: Button, previews

--- app/src/main/java/com/rick/jetsnack_ui/ui/components/Button.kt
+++ app/src/main/java/com/rick/jetsnack_ui/ui/components/Button.kt
@@ -1,5 +1,6 @@
 package com.rick.jetsnack_ui.ui.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
+import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -17,8 +19,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
 import com.rick.jetsnack_ui.ui.theme.JetsnackTheme
 import com.rick.jetsnack_ui.ui.theme.Shapes
 
@@ -55,7 +59,11 @@ fun JetsnackButton(
     ) {
         ProvideTextStyle(value = MaterialTheme.typography.button) {
             Row(
-                Modifier.defaultMinSize(minWidth = ButtonDefaults.MinWidth, minHeight = ButtonDefaults.MinHeight)
+                Modifier
+                    .defaultMinSize(
+                        minWidth = ButtonDefaults.MinWidth,
+                        minHeight = ButtonDefaults.MinHeight
+                    )
                     .indication(interactionSource, rememberRipple())
                     .padding(contentPadding),
                 horizontalArrangement = Arrangement.Center,
@@ -63,6 +71,30 @@ fun JetsnackButton(
                 content = content
             )
 
+        }
+    }
+}
+
+@Preview("default", "round")
+@Preview("dark theme", "round", uiMode = UI_MODE_NIGHT_YES)
+@Preview("large font", "round", fontScale = 2f)
+@Composable
+fun ButtonPreview() {
+    JetsnackTheme {
+        JetsnackButton(onClick = {}) {
+            Text(text = "Demo")
+        }
+    }
+}
+
+@Preview("default", "rectangle")
+@Preview("dark theme", "rectangle", uiMode = UI_MODE_NIGHT_YES)
+@Preview("large font", "rectangle", fontScale = 2f)
+@Composable
+fun RectButtonPrev() {
+    JetsnackTheme {
+        JetsnackButton(onClick = { /*TODO*/ }, shape = RectangleShape) {
+            Text(text = "Demo")
         }
     }
 }
