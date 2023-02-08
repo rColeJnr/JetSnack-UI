0f426e7ef878ce886778f83a9de61858bdfae479
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-08 13:35:29 +0300

    Components: Snackbar

---
+++ app/src/main/java/com/rick/jetsnack_ui/ui/components/Snackbar.kt
@@ -0,0 +1,35 @@
+package com.rick.jetsnack_ui.ui.components
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarData
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.rick.jetsnack_ui.ui.theme.JetsnackTheme
+
+@Composable
+fun JetsnackSnackbar(
+    snackbarData: SnackbarData,
+    modifier: Modifier = Modifier,
+    actionOnNewLine: Boolean = false,
+    shape: Shape = MaterialTheme.shapes.small,
+    backgroundColor: Color = JetsnackTheme.colors.uiBackground,
+    contentColor: Color = JetsnackTheme.colors.textSecondary,
+    actionColor: Color = JetsnackTheme.colors.brand,
+    elevation: Dp = 6.dp
+) {
+    Snackbar(
+        snackbarData = snackbarData,
+        modifier = modifier,
+        actionOnNewLine = actionOnNewLine,
+        shape = shape,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        actionColor = actionColor,
+        elevation = elevation
+    )
+}
